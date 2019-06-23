5b6aba24cb282aaba39409e2002b16007247d62f
karenvargasr <karen.vargas.r@gmail.com>
2019-06-23 02:09:23 -0400

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,47 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+
+  encode: (text, secretPass) => {
+    let result ='';
+    for (let i=0 ; i < text.length ; i++ ){
+    let formula = text.charCodeAt(i)
+      //MAYUSCULAS
+      if(formula >= 65 && formula <=  90) {
+      result += String.fromCharCode ((formula - 65 + parseInt(secretPass)) % 26 + 65)
+      }
+      //Minusculas
+      if(formula >= 97 && formula <=  122) {
+      result += String.fromCharCode ((formula - 97 + parseInt(secretPass)) % 26 + 97)
+      }
+      //ESPACIO
+      if(formula >= 32 && formula <= 32) {
+      result += String.fromCharCode ((formula - 32 + secretPass) % 1 + 32)
+      }
+      //NUMEROS
+      if(formula >= 48 && formula <=  57) {
+        result += String.fromCharCode(((formula - 48 + parseInt(secretPass)+20) % 10) + 48)
+      }
+    } return result
+},
+decode: (text, secretPass) => {
+  let result='';
+for (let i=0 ; i < text.length ; i++ ){
+  let formula = text.charCodeAt(i)
+    //MAYUSCULAS
+    if(formula >= 65 && formula <=  90) {
+    result += String.fromCharCode ((formula - 65 + parseInt(secretPass)) % 26 + 65)
+    }
+    //Minusculas
+    if(formula >= 97 && formula <=  122) {
+    result += String.fromCharCode ((formula - 97 + parseInt(secretPass)) % 26 + 97)
+    }
+    //ESPACIO
+    if(formula >= 32 && formula <= 32) {
+    result += String.fromCharCode ((formula - 32 + secretPass) % 1 + 32)
+    }
+    //NUMEROS
+    if(formula >= 48 && formula <=  57) {
+      result += String.fromCharCode(((formula - 48 + parseInt(secretPass)+20) % 10) + 48)
+    }
+  } return result
+} 
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-/*Acá va tu código*/
+const btnCipher = document.getElementById('cipher');
+const btnDecipher = document.getElementById('decipher');
+
+const containerResult = document.getElementById('result');
+
+btnCipher.addEventListener('click', () => {
+  let text = document.getElementById('enterText').value;
+  let numOffset = document.getElementById('secretPass').value;
+
+  containerResult.innerHTML = window.cipher.encode(text, numOffset);
+
+})
+btnDecipher.addEventListener('click', () => {
+  let text = document.getElementById('enterText').value;
+  let numOffset = document.getElementById('secretPass').value;
+
+  containerResult.innerHTML = window.cipher.decode(text, numOffset);
+  })
+
