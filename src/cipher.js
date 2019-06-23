88e26fca8c3b323a4dbfa73bbc6d449f520d5604
karenvargasr <karen.vargas.r@gmail.com>
2019-06-23 19:44:26 -0400

    decifra y cifra

--- src/cipher.js
+++ src/cipher.js
@@ -6,19 +6,19 @@ window.cipher = {
     let formula = text.charCodeAt(i)
       //Mayusculas: A se encuentra en el lugar 65 ASCII
       if(formula >= 65 && formula <=  90) {
-      result += String.fromCharCode ((formula - 65 + parseInt(numOffset)) % 26 + 65)
+      result +=String.fromCharCode ((formula - 65 + parseInt(numOffset)) % 26 + 65)
       }
       //Minuscula: minucsulas se encuentran en el 97 al 122 ASCII
       if(formula >= 97 && formula <=  122) {
-      result += String.fromCharCode ((formula - 97 + parseInt(numOffset)) % 26 + 97)
+      result +=String.fromCharCode ((formula - 97 + parseInt(numOffset)) % 26 + 97)
       }
       //Espacios: el espacio va en el 32, por lo tanto se mantiene 
-      if(formula == 32 && formula <= 32) {
-      result += String.fromCharCode ((formula - 32 + numOffset) % 1 + 32)
+      if(formula >= 32 && formula <= 32) {
+      result +=String.fromCharCode ((formula - 32 + numOffset) % 1 + 32)
       }
       //NUMEROS: se encuentran desde el 48 al 57 en ASCII
       if(formula >= 48 && formula <=  57) {
-        result += String.fromCharCode(((formula - 48 + parseInt(numOffset)+20) % 10) + 48)
+        result +=String.fromCharCode(((formula - 48 + parseInt(numOffset)+20) % 10) + 48)
       }
     } return result
 },
@@ -28,19 +28,19 @@ for (let i=0 ; i < text.length ; i++ ){
   let formula = text.charCodeAt(i)
     //MAYUSCULAS
     if(formula >= 65 && formula <=  90) {
-    result += String.fromCharCode ((formula - 65 + parseInt(numOffset)) % 26 + 65)
+      result +=String.fromCharCode ((formula - 65 - parseInt(numOffset)) % 26 + 65)
     }
     //minusculas
     if(formula >= 97 && formula <=  122) {
-    result += String.fromCharCode ((formula - 97 + parseInt(numOffset)) % 26 + 97)
+    result +=String.fromCharCode ((formula - 97 - parseInt(numOffset)) % 26 + 97)
     }
     //ESPACIO
     if(formula >= 32 && formula <= 32) {
-    result += String.fromCharCode ((formula - 32 + numOffset) % 1 + 32)
+    result +=String.fromCharCode ((formula - 32 - numOffset) % 1 + 32)
     }
     //NUMEROS
     if(formula >= 48 && formula <=  57) {
-      result += String.fromCharCode(((formula - 48 + parseInt(numOffset)+20) % 10) + 48)
+      result +=String.fromCharCode(((formula - 48 - parseInt(numOffset)+20) % 10) + 48)
     }
   } return result
 } 
